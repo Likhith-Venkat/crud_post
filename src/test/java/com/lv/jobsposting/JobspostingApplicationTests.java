4f520a35d2e0bd830ffd8bf5e8c3a699c3f4c0f7
Likhith Venkat Pagadala <likhithvenkat.pagadala@m2pfintech.com>
2024-05-17 13:20:15 +0530

    test 1

--- src/test/java/com/lv/jobsposting/JobspostingApplicationTests.java
+++ src/test/java/com/lv/jobsposting/JobspostingApplicationTests.java
@@ -4,7 +4,7 @@ import com.lv.jobsposting.controller.PostController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes= PostController.class)
+@SpringBootTest(classes = {PostController.class})
 class JobspostingApplicationTests {
 
 	@Test
